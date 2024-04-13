151545858cfc21a170287490ae65c158da5f705a
Andres Morey <andresmarcel@gmail.com>
2024-04-13 12:45:58 +0300

    added linter, fixed linting issues

--- src/config.test.ts
+++ src/config.test.ts
@@ -1,45 +1,42 @@
-import { Config } from './config'
+import { Config } from './config';
+import type { ConfigOptions } from './config';
 
 describe('Config tests', () => {
-  const initConfigFn = (opts) => {
-    return () => {
-      return new Config(opts)
-    }
-  }
-  
+  const initConfigFn = (opts: Partial<ConfigOptions>) => () => new Config(opts);
+
   it('returns default config when options are absent', () => {
-    const config = new Config()
-    expect(config.cookie.domain).toEqual('')
-    expect(config.cookie.httpOnly).toEqual(true)
-    expect(config.cookie.maxAge).toEqual(undefined)
-    expect(config.cookie.name).toEqual('_csrfSecret')
-    expect(config.cookie.path).toEqual('/')
-    expect(config.cookie.sameSite).toEqual('strict')
-    expect(config.cookie.secure).toEqual(true)
-    expect(config.ignoreMethods).toEqual(['GET', 'HEAD', 'OPTIONS'])
-    expect(config.saltByteLength).toEqual(8)
-    expect(config.secretByteLength).toEqual(18)
-    expect(config.token.responseHeader).toEqual('X-CSRF-Token')
-    expect(config.token.value).toEqual(undefined)
-  })
+    const config = new Config();
+    expect(config.cookie.domain).toEqual('');
+    expect(config.cookie.httpOnly).toEqual(true);
+    expect(config.cookie.maxAge).toEqual(undefined);
+    expect(config.cookie.name).toEqual('_csrfSecret');
+    expect(config.cookie.path).toEqual('/');
+    expect(config.cookie.sameSite).toEqual('strict');
+    expect(config.cookie.secure).toEqual(true);
+    expect(config.ignoreMethods).toEqual(['GET', 'HEAD', 'OPTIONS']);
+    expect(config.saltByteLength).toEqual(8);
+    expect(config.secretByteLength).toEqual(18);
+    expect(config.token.responseHeader).toEqual('X-CSRF-Token');
+    expect(config.token.value).toEqual(undefined);
+  });
 
   it('saltByteLength must be greater than 0', () => {
-    expect(initConfigFn({saltByteLength: 0})).toThrow(Error)
-    expect(initConfigFn({saltByteLength: 1})).not.toThrow(Error)
-  })
+    expect(initConfigFn({ saltByteLength: 0 })).toThrow(Error);
+    expect(initConfigFn({ saltByteLength: 1 })).not.toThrow(Error);
+  });
 
   it('saltByteLength must be less than 256', () => {
-    expect(initConfigFn({saltByteLength: 256})).toThrow(Error)
-    expect(initConfigFn({saltByteLength: 255})).not.toThrow(Error)
-  })
-  
+    expect(initConfigFn({ saltByteLength: 256 })).toThrow(Error);
+    expect(initConfigFn({ saltByteLength: 255 })).not.toThrow(Error);
+  });
+
   it('secretByteLength must be greater than 0', () => {
-    expect(initConfigFn({secretByteLength: 0})).toThrow(Error)
-    expect(initConfigFn({secretByteLength: 1})).not.toThrow(Error)
-  })
+    expect(initConfigFn({ secretByteLength: 0 })).toThrow(Error);
+    expect(initConfigFn({ secretByteLength: 1 })).not.toThrow(Error);
+  });
 
   it('secretByteLength must be less than 256', () => {
-    expect(initConfigFn({secretByteLength: 256})).toThrow(Error)
-    expect(initConfigFn({secretByteLength: 255})).not.toThrow(Error)
-  })
-})
+    expect(initConfigFn({ secretByteLength: 256 })).toThrow(Error);
+    expect(initConfigFn({ secretByteLength: 255 })).not.toThrow(Error);
+  });
+});
--- src/config.ts
+++ src/config.ts
@@ -1,49 +1,61 @@
 import type { TokenValueFunction } from './util';
 
 export class CookieOptions {
-  domain: string = ''
-  httpOnly: boolean = true
-  maxAge: number | undefined = undefined
-  name: string = '_csrfSecret'
-  path: string = '/'
-  sameSite: boolean | 'none' | 'strict' | 'lax' = 'strict'
-  secure: boolean = true
-  
+  domain: string = '';
+
+  httpOnly: boolean = true;
+
+  maxAge: number | undefined = undefined;
+
+  name: string = '_csrfSecret';
+
+  path: string = '/';
+
+  sameSite: boolean | 'none' | 'strict' | 'lax' = 'strict';
+
+  secure: boolean = true;
+
   constructor(opts?: Partial<CookieOptions>) {
     Object.assign(this, opts);
   }
 }
 
 export class TokenOptions {
-  responseHeader: string = 'X-CSRF-Token'
-  value: TokenValueFunction | undefined = undefined
-  
+  responseHeader: string = 'X-CSRF-Token';
+
+  value: TokenValueFunction | undefined = undefined;
+
   constructor(opts?: Partial<TokenOptions>) {
     Object.assign(this, opts);
   }
 }
 
 export class Config {
-  cookie: CookieOptions = new CookieOptions()
-  excludePathPrefixes: string[] = ['/_next/']
-  ignoreMethods: string[] = ['GET', 'HEAD', 'OPTIONS']
-  saltByteLength: number = 8
-  secretByteLength: number = 18
-  token: TokenOptions = new TokenOptions()
-  
+  cookie: CookieOptions = new CookieOptions();
+
+  excludePathPrefixes: string[] = ['/_next/'];
+
+  ignoreMethods: string[] = ['GET', 'HEAD', 'OPTIONS'];
+
+  saltByteLength: number = 8;
+
+  secretByteLength: number = 18;
+
+  token: TokenOptions = new TokenOptions();
+
   constructor(opts?: Partial<ConfigOptions>) {
-    opts = opts || {}
-    if (opts.cookie) opts.cookie = new CookieOptions(opts.cookie);
-    if (opts.token) opts.token = new TokenOptions(opts.token);
-    Object.assign(this, opts)
+    const newOpts = opts || {};
+    if (newOpts.cookie) newOpts.cookie = new CookieOptions(newOpts.cookie);
+    if (newOpts.token) newOpts.token = new TokenOptions(newOpts.token);
+    Object.assign(this, opts);
 
     // basic validation
     if (this.saltByteLength < 1 || this.saltByteLength > 255) {
-      throw Error('saltBytLength must be greater than 0 and less than 256')
+      throw Error('saltBytLength must be greater than 0 and less than 256');
     }
 
     if (this.secretByteLength < 1 || this.secretByteLength > 255) {
-      throw Error('secretBytLength must be greater than 0 and less than 256')
+      throw Error('secretBytLength must be greater than 0 and less than 256');
     }
   }
 }
@@ -51,4 +63,4 @@ export class Config {
 export interface ConfigOptions extends Omit<Config, 'cookie' | 'token'> {
   cookie: Partial<CookieOptions>;
   token: Partial<TokenOptions>;
-};
+}
