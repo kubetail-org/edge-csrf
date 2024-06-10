deb35e8acfd980431880f7ca3e10892d457d0ab2
Andres Morey <andresmarcel@gmail.com>
2024-06-10 12:29:51 +0300

    wip

--- packages/express/src/index.ts
+++ packages/express/src/index.ts
@@ -14,7 +14,7 @@ export { CsrfError };
 function getRequestBody(req: ExpressRequest): Promise<string> {
   return new Promise((resolve, reject) => {
     let body = '';
-    req.on('data', (chunk) => body += chunk.toString());
+    req.on('data', (chunk) => { body += chunk.toString(); });
     req.on('end', () => resolve(body));
     req.on('error', (err) => reject(err));
   });
