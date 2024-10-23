9115d74219e8669c9e7f896fd13557346441e720
Stef <git@sril.email>
2024-10-23 13:49:51 +0100

    Support next v15 (#70)
    * Support next v15
    * Add next15 examples
    * Fixes for async/await
    * Upgrade eslint-config-next
    * Bump next
    * Update pnpm-lock.yaml
    * Update configs
    * Small changes to readmes
    
    ---------
    
    Co-authored-by: Andres Morey <andresmarcel@gmail.com>

---
+++ examples/next15-approuter-server-action-form-submission/app/page.tsx
@@ -0,0 +1,70 @@
+import { revalidatePath } from 'next/cache';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+import '../styles/globals.css';
+
+export default async function Page() {
+  const headersList = await headers();
+  const csrfToken = headersList.get('X-CSRF-Token') || 'missing';
+
+  async function myAction() {
+    'use server';
+
+    // eslint-disable-next-line no-console
+    console.log('passed csrf validation');
+    revalidatePath('/');
+    redirect('/');
+  }
+
+  return (
+    <>
+      <p>
+        CSRF token value:
+        {csrfToken}
+      </p>
+      <h2>Server Action Form Submission Example:</h2>
+      <p>NOTE: Look at browser network logs and server console for submission feedback</p>
+      <h3>Example 1:</h3>
+      <form action={myAction}>
+        <legend>Form without CSRF (should fail):</legend>
+        <input type="text" name="input1" />
+        <button type="submit">Submit</button>
+      </form>
+      <br />
+      <form action={myAction}>
+        <legend>Form with incorrect CSRF (should fail):</legend>
+        <input type="hidden" name="csrf_token" value="notvalid" />
+        <input type="text" name="input1" />
+        <button type="submit">Submit</button>
+      </form>
+      <br />
+      <form action={myAction}>
+        <legend>Form with CSRF (should succeed):</legend>
+        <input type="hidden" name="csrf_token" value={csrfToken} />
+        <input type="text" name="input1" />
+        <button type="submit">Submit</button>
+      </form>
+      <h3>Example 2:</h3>
+      <form action={myAction}>
+        <legend>Form without CSRF (should fail):</legend>
+        <input type="file" name="file1" />
+        <button type="submit">Submit</button>
+      </form>
+      <br />
+      <form action={myAction}>
+        <legend>Form with incorrect CSRF (should fail):</legend>
+        <input type="hidden" name="csrf_token" value="notvalid" />
+        <input type="file" name="file1" />
+        <button type="submit">Submit</button>
+      </form>
+      <br />
+      <form action={myAction}>
+        <legend>Form with CSRF (should succeed):</legend>
+        <input type="hidden" name="csrf_token" value={csrfToken} />
+        <input type="file" name="file1" />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+}
